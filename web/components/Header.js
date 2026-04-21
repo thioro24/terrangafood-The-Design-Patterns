7269067e2379c3b9a4cdd1547bd59929c3bcc382
Fatou SY <fsy67698@gmail.com>
2026-04-21 19:23:08 +0200

    feat : ajout contributeur Fatou + Ajouter le nom de l’équipe dans le composant Header.js du frontend.

--- web/components/Header.js
+++ web/components/Header.js
@@ -3,7 +3,10 @@ import Link from 'next/link';
 export default function Header() {
   return (
     <header className="header">
+      <h1 className="header-title">Bienvenue sur TerrangaFood</h1>
+      <p className="header-subtitle">The-Disign-Patterns</p>
       <div className="header-content">
+
         <Link href="/" className="header-logo">
           🍛 Terranga<span>Food</span>
         </Link>
